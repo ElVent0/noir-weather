1092c1c3abe89fe55326251501380996a618ba85
Vasyl Tarasyuk <vasfreelan@gmail.com>
2022-11-10 19:27:12 +0200

    temporary fix (does not work with ukraine)

--- src/api/apiWeather.jsx
+++ src/api/apiWeather.jsx
@@ -1,26 +1,32 @@
-let lat = 50.4823808;
-let lon = 30.3562752;
+// let lat = 50.4823808;
+// let lon = 30.3562752;
 
-const setLatLon = (position) => {
-  lat = position.coords.latitude;
-  lon = position.coords.longitude;
-};
+// const setLatLon = (position) => {
+//   lat = position.coords.latitude;
+//   lon = position.coords.longitude;
+// };
 
-navigator.geolocation.getCurrentPosition(setLatLon);
+// navigator.geolocation.getCurrentPosition(setLatLon);
+
+// console.log(parseFloat(lat), parseFloat(lon));
+
+// API передумав давати дані про Україну, тому поки закоментував можливість визначення локації юзера
 
 export const getData = async () => {
   const data = await fetch(
-    `https://api.open-meteo.com/v1/forecast?latitude=${parseFloat(
-      lat
-    )}&longitude=${parseFloat(
-      lon
-    )}&hourly=temperature_2m,relativehumidity_2m,apparent_temperature,precipitation,rain,showers,snowfall,weathercode,surface_pressure,cloudcover,windspeed_10m,winddirection_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset&current_weather=true&timezone=auto`,
-    {
-      mode: "cors",
-      cache: "no-cache",
-    }
+    // `https://api.open-meteo.com/v1/forecast?latitude=${parseFloat(
+    //   lat
+    // )}&longitude=${parseFloat(
+    //   lon
+    // )}&hourly=temperature_2m,relativehumidity_2m,apparent_temperature,precipitation,rain,showers,snowfall,weathercode,surface_pressure,cloudcover,windspeed_10m,winddirection_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset&current_weather=true&timezone=auto`
+    `https://api.open-meteo.com/v1/forecast?latitude=40.71&longitude=-74.01&hourly=temperature_2m,relativehumidity_2m,apparent_temperature,precipitation,rain,showers,snowfall,weathercode,surface_pressure,cloudcover,windspeed_10m,winddirection_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset&current_weather=true&timezone=auto`
+    // {
+    //   mode: "cors",
+    //   cache: "no-cache",
+    // }
   );
   const parsedData = await data.json();
+  console.log(parsedData);
   return parsedData;
 };
 
@@ -30,11 +36,11 @@ export const getDataFromSearch = async (lat, lon) => {
       lat
     )}&longitude=${parseFloat(
       lon
-    )}&hourly=temperature_2m,relativehumidity_2m,apparent_temperature,precipitation,rain,showers,snowfall,weathercode,surface_pressure,cloudcover,windspeed_10m,winddirection_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset&current_weather=true&timezone=auto`,
-    {
-      mode: "cors",
-      cache: "no-cache",
-    }
+    )}&hourly=temperature_2m,relativehumidity_2m,apparent_temperature,precipitation,rain,showers,snowfall,weathercode,surface_pressure,cloudcover,windspeed_10m,winddirection_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset&current_weather=true&timezone=auto`
+    // {
+    //   mode: "cors",
+    //   cache: "no-cache",
+    // }
   );
   const parsedData = await data.json();
   return parsedData;
--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,11 @@
 import Content from "./Content/Content";
-
 import { Routes, Route } from "react-router-dom";
+import { Notify } from "notiflix/build/notiflix-notify-aio";
 
 const App = () => {
+  Notify.info(
+    "API stopped distributing data for Ukraine. Do not try to search this region, it will break the site"
+  );
   return (
     <Routes>
       <Route path="/" element={<Content />}></Route>
